c98067d810f088da7e47f7eb1138a41b57cd1b05
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2023-05-04 16:46:42 -0300

    novos especis e fotos

--- classes/Veiculo.php
+++ classes/Veiculo.php
@@ -2,10 +2,10 @@
 
     class Veiculo{
 
-    public static function cadastrarVeiculo($nomev, $preco, $cambio, $km, $combustivel, $imagem1, $imagem2, $imagem3, $espec1, $espec2, $espec3, $espec4)
+    public static function cadastrarVeiculo($nomev, $preco, $cambio, $km, $combustivel, $imagem1, $imagem2, $imagem3, $imagem4, $imagem5, $espec1, $espec2, $espec3, $espec4, $espec5, $espec6, $espec7, $espec8, $espec9, $espec10)
     {
-        $sql = MySql::conectar()->prepare("INSERT INTO `veiculos` VALUES (null,?,?,?,?,?,?,?,?,?,?,?,?)");
-        $sql->execute(array($nomev, $preco, $cambio, $km, $combustivel, $imagem1, $imagem2, $imagem3, $espec1, $espec2, $espec3, $espec4));
+        $sql = MySql::conectar()->prepare("INSERT INTO `veiculos` VALUES (null,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+        $sql->execute(array($nomev, $preco, $cambio, $km, $combustivel, $imagem1, $imagem2, $imagem3, $imagem4, $imagem5, $espec1, $espec2, $espec3, $espec4, $espec5, $espec6, $espec7, $espec8, $espec9, $espec10));
     }
     }
 
--- painel/pages/adicionarVeiculo.php
+++ painel/pages/adicionarVeiculo.php
@@ -10,7 +10,7 @@
 </style>
 <br><br>
 <h3>Inserção de veiculos</h3>
-<br>
+
 <form method="post" enctype="multipart/form-data">
 
 
@@ -25,11 +25,20 @@
         $imagem1 = $_FILES['fotoUm'];
         $imagem2 = $_FILES['fotoDois'];
         $imagem3 = $_FILES['fotoTres'];
+        $imagem4 = $_FILES['fotoQuatro'];
+        $imagem5 = $_FILES['fotoCinco'];
 
         $espec1 = $_POST['txtEspe1'];
         $espec2 = $_POST['txtEspe2'];
         $espec3 = $_POST['txtEspe3'];
         $espec4 = $_POST['txtEspe4'];
+        $espec5 = $_POST['txtEspe5'];
+        $espec6 = $_POST['txtEspe6'];
+        $espec7 = $_POST['txtEspe7'];
+        $espec8 = $_POST['txtEspe8'];
+        $espec9 = $_POST['txtEspe9'];
+        $espec10 = $_POST['txtEspe10'];
+
 
         $veiculo = new Veiculo();
 
@@ -38,8 +47,10 @@
         $imagem1 = Painel::uploadFile($imagem1);
         $imagem2 = Painel::uploadFile($imagem2);
         $imagem3 = Painel::uploadFile($imagem3);
-        $veiculo->cadastrarVeiculo($nomev, $preco, $cambio, $km, $combustivel, $imagem1, $imagem2, $imagem3, $espec1, $espec2, $espec3, $espec4);
-        echo '<script>alert("Veículo inserido com sucesso!</script>")';
+        $imagem4 = Painel::uploadFile($imagem4);
+        $imagem5 = Painel::uploadFile($imagem5);
+        $veiculo->cadastrarVeiculo($nomev, $preco, $cambio, $km, $combustivel, $imagem1, $imagem2, $imagem3, $imagem4, $imagem5, $espec1, $espec2, $espec3, $espec4, $espec5, $espec6, $espec7, $espec8, $espec9, $espec10);
+        echo '<script>alert("Veículo inserido com sucesso!")</script>';
     }
     ?>
 
@@ -74,19 +85,29 @@
     <br>
 
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-2" style="width: 20%;">
             <label>Foto(1):</label><span style="color:red"> *</span>
             <input class="form-control" type="file" name="fotoUm" id="fotoUm" onblur="validarForm()">
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-2" style="width: 20%;">
             <label>Foto(2):</label>
             <input class="form-control" type="file" name="fotoDois" id=" fotoDois">
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-2" style="width: 20%;">
             <label>Foto(3):</label>
             <input class="form-control" type="file" name="fotoTres" id="fotoTres">
+        </div>
+
+        <div class="col-md-2" style="width: 20%;">
+            <label>Foto(4):</label>
+            <input class="form-control" type="file" name="fotoQuatro" id="fotoQuatro">
+        </div>
+
+        <div class="col-md-2" style="width: 20%;">
+            <label>Foto(5):</label>
+            <input class="form-control" type="file" name="fotoCinco" id="fotoCinco">
         </div>
     </div>
 
@@ -115,6 +136,44 @@
     </div>
 
     <br>
+
+    <div class="row">
+        <div class="col-md-3">
+            <label>Especificações(5)</label>
+            <input class="form-control" type="text" name="txtEspe5" id="txtEspe5">
+        </div>
+
+        <div class="col-md-3">
+            <label>Especificações(6)</label>
+            <input class="form-control" type="text" name="txtEspe6" id="txtEspe6">
+        </div>
+
+        <div class="col-md-3">
+            <label>Especificações(7)</label>
+            <input class="form-control" type="text" name="txtEspe7" id="txtEspe7">
+        </div>
+
+        <div class="col-md-3">
+            <label>Especificações(8):</label>
+            <input class="form-control" type="text" name="txtEspe8" id="txtEspe8">
+        </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+        <div class="col-md-3">
+            <label>Especificações(9)</label>
+            <input class="form-control" type="text" name="txtEspe9" id="txtEspe9">
+        </div>
+
+        <div class="col-md-3">
+            <label>Especificações(10)</label>
+            <input class="form-control" type="text" name="txtEspe10" id="txtEspe10">
+        </div>
+    </div>
+
+
 
     <div class="row" style="justify-content: flex-end">
         <div class="col-md-3">
